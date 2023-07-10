d0f7cfe27b9219d32426a3211a87e695d5e37297
Delson Magalhaes <dl_magalhaes@hotmail.com>
2023-07-10 11:07:13 -0300

    Atualizando props

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ToDo from './components/ToDo'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const tarefas = ['Alimentar cachorro','Passear com cachorro','Tomar café', 'Escovar os dentes','Tomar banho']
+    // const tarefas = "TarefasXXXXXXXX"
+
+    return (
+      <>
+        <h1>Exercícios semana 6</h1>
+            <ToDo tarefas={tarefas}/>
+      </>
+    )
+
 }
 
 export default App
--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,7 +1,18 @@
-function Todo(){
+import React from "react"
+
+
+function ToDo({tarefas}){
+    
+
     return(
-        <>
-        
-        </>
+        <div>
+            <ul>
+                {tarefas.map(item=>{
+                    return <li key={item}>{item}</li>
+                })}
+
+            </ul>        
+        </div>
     )
 }
+export default ToDo
