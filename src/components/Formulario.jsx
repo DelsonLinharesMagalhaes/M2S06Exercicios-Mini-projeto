18a1297d9f166fb2bbe00d280547bf8c601de897
Delson Magalhaes <dl_magalhaes@hotmail.com>
2023-07-12 17:02:18 -0300

    Exercicios semana 7final

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,20 +1,30 @@
-import { useState } from "react"
+import { useState, useRef } from "react"
 
 function Formulario({handleSubmit}){
 
     const [tarefa, setTarefa] = useState();
+    const [error, setError] = useState();
+    const inputRef = useRef(null);
     const handleTarefaChange = (event) => {setTarefa(event.target.value)}
 
-    const criarTarefa = (event) => { event.preventDefault(), handleSubmit(tarefa)}
+    const criarTarefa = (event) => { event.preventDefault();
+        tarefa.trim() === "" ? (setError("O campo não pode ser vazio"), inputRef.current.focus()): null;
+        handleSubmit(tarefa)
+        setTarefa("");
+        setError("");
+    }
 
     
 
     return(
         <form onSubmit={criarTarefa}>
-            <input value={tarefa} onChange={handleTarefaChange} placeholder="Digite uma nova tarefa" type="text"/>
+            <input value={tarefa} onChange={handleTarefaChange} placeholder="Digite uma nova tarefa" type="text" ref={inputRef}/>
+            {error&&<div>{error}</div>}
             <button type='submit'>Criar Tarefa</button>
         </form>
     )
 }
 
 export default Formulario
+
+
